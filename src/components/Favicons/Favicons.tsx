6b4d3bdf78cbd82c284155671a26e08f29c28a21
Lwin Moe Paing <lwinmoepaing.dev@gmail.com>
2023-10-14 13:22:29 +0630

    :sparkles: feat(favicon): adding favicon for all cross platform

---
+++ src/components/Favicons/Favicons.tsx
@@ -0,0 +1,39 @@
+const Favicons = () => {
+  return (
+    <>
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/apple-touch-icon.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="192x192"
+        href="/android-chrome-192x192.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/favicon-16x16.png"
+      />
+      <link rel="manifest" href="/site.webmanifest" />
+      <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#923293" />
+      <meta
+        name="apple-mobile-web-app-title"
+        content="Myanmar Software Engineers"
+      />
+      <meta name="application-name" content="Myanmar Software Engineers" />
+      <meta name="msapplication-TileColor" content="#923293" />
+      <meta name="theme-color" content="#923293" />
+    </>
+  );
+};
+export default Favicons;
