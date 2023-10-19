9836bdb01191d479b95dd9b64177a1266b943759
Lwin Moe Paing <lwinmoepaing.dev@gmail.com>
2023-10-19 15:40:57 +0630

    :sparkles: feat(pwa): adding pwa for mmswe

--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
 const { withContentlayer } = require("next-contentlayer");
-const path = require("path");
+const withPwa = require("next-pwa");
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  ...withPwa({
+    dest: "public",
+    register: true,
+    skipWaiting: true,
+  }),
   reactStrictMode: true,
   swcMinify: true,
   output: "export",
