183836e57aeb59e0a34a3ab789195ac665be708a
edotheDev <myathtethlaing@kbtc.edu.mm>
2023-10-22 04:38:50 +0630

    :fire: build(profile): add edo profile

---
+++ content/profile/edothedev/Edo.jsx
@@ -0,0 +1,7 @@
+import Spline from '@splinetool/react-spline';
+
+export default function App() {
+    return (
+        <Spline scene="https://prod.spline.design/523zxk5lcIEjBpbL/scene.splinecode" />
+    );
+}
