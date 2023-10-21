cfc9849af4368e0d792a3ddc4318680475f2494a
Lwin Moe Paing <lwinmoepaing.dev@gmail.com>
2023-10-21 23:07:16 +0630

    :bug: fix(profile): sperate profile list and profile item component

--- src/components/Contributors/Counter/Counter.tsx
+++ src/components/Contributors/Counter/Counter.tsx
@@ -1,12 +1,16 @@
 import SquareBox from "@/components/Ui/SquareBox/SquareBox";
+import { generateColor } from "@/utils";
 import { useState } from "react";
 
 const Counter = () => {
   const [counter, setState] = useState(0);
   return (
-    <SquareBox className="bg-rose-500">
-      <div className="cursor-pointer" onClick={() => setState((prev) => prev + 1)}>
-        Click Me: {counter}
+    <SquareBox className={generateColor()}>
+      <div
+        className="cursor-pointer"
+        onClick={() => setState((prev) => prev + 1)}
+      >
+        Counter with JS: {counter}
       </div>
     </SquareBox>
   );
--- src/components/Profile/ProfileCardList/ProfileCardList.tsx
+++ src/components/Profile/ProfileCardList/ProfileCardList.tsx
@@ -1,65 +1,19 @@
 "use client";
 
 import SpacingDivider from "@/components/Common/SpacingDivider/SpacingDivider";
+import Tag from "@/components/Common/Tag/Tag";
 import TitleText from "@/components/Common/TitleText/TitleText";
-import SquareBox from "@/components/Ui/SquareBox/SquareBox";
+import ProfileCardItem from "@/components/Profile/ProfileCardItem/ProfileCardItem";
 import { titleFont } from "@/fonts/fonts";
-import { cn, generateColor, generateColorArray } from "@/utils";
-import { checkIsFoundTag, profileHelperService } from "@/utils/profileHelper";
+import { cn, generateColorArray } from "@/utils/index";
+import { profileHelperService } from "@/utils/profileHelper";
 import { Profile } from "contentlayer/generated";
-import Image from "next/image";
-import Link from "next/link";
-import { useRouter, useSearchParams } from "next/navigation";
-import { MouseEvent, useCallback, useMemo, useState } from "react";
-import { IoClose, IoPeople } from "react-icons/io5";
+import { useSearchParams } from "next/navigation";
+import { useMemo, useState } from "react";
+import { IoPeople } from "react-icons/io5";
 
 type TPropsProfileCardList = {
   profiles: Profile[];
-};
-
-const Tag = ({
-  tag,
-  searchTag,
-  bgColor,
-  isShowClose = false,
-}: {
-  tag: string;
-  searchTag: string;
-  bgColor: string;
-  isShowClose?: boolean;
-}) => {
-  const searchParams = useSearchParams();
-  const router = useRouter();
-  const isTagActive = checkIsFoundTag(tag, searchTag);
-
-  const onClickTag = useCallback(
-    (e: MouseEvent) => {
-      e.preventDefault();
-      const tmpSearchParam = new URLSearchParams(searchParams.toString());
-
-      tmpSearchParam.set("tag", !isTagActive ? tag : "");
-
-      router.push(`/profile?${tmpSearchParam.toString()}`);
-    },
-    [isTagActive, router, searchParams, tag]
-  );
-
-  return (
-    <TitleText
-      className={cn(
-        "inline-block cursor-pointer text-[10px] px-2 py-1 rounded-full mb-1 mr-[5px] bg-opacity-70 hover:bg-opacity-90",
-        bgColor,
-        isTagActive &&
-          "bg-green-600 bg-opacity-100 outline-dashed outline-2 outline-offset-2 relative"
-      )}
-      key={tag}
-      tag="span"
-      onClick={onClickTag}
-    >
-      {tag}
-      {isShowClose ? <IoClose /> : null}
-    </TitleText>
-  );
 };
 
 const ProfileCardList = ({ profiles }: TPropsProfileCardList) => {
@@ -114,7 +68,7 @@ const ProfileCardList = ({ profiles }: TPropsProfileCardList) => {
 
         {searchedTags.map((tag, i) => (
           <Tag
-            key={tag}
+            key={`distincted_${tag}`}
             tag={tag}
             searchTag={searchTag}
             bgColor={uniqueTagColors[i]}
@@ -125,63 +79,18 @@ const ProfileCardList = ({ profiles }: TPropsProfileCardList) => {
       <SpacingDivider size="lg" />
 
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-5">
-        {filteredProfiles.map((profile) => {
-          const bgColor = generateColor();
-
-          return (
-            <div key={profile._id} className="self-stretch">
-              <Link href={`/profile/${profile.slugAsParams}`}>
-                <SquareBox
-                  className={cn(
-                    "w-full h-full transition ease-out cursor-pointer hover:opacity-80 hover:-translate-y-1 bg-opacity-40 min-h-[130px]",
-                    bgColor
-                  )}
-                >
-                  <div className="flex flex-row items-center mb-2 space-x-2">
-                    <div
-                      className={cn(
-                        "flex justify-center items-center h-10 w-10 rounded-full overflow-hidden relative",
-                        bgColor
-                      )}
-                    >
-                      {!!profile.image ? (
-                        <Image
-                          src={profile.image}
-                          className="h-10 w-10 object-fit"
-                          alt={profile.name}
-                          fill
-                        />
-                      ) : (
-                        profile.name?.trim()?.[0]
-                      )}
-                    </div>
-                    <div className="flex-1">
-                      <TitleText tag="h4" className="text-base">
-                        {profile.name}
-                      </TitleText>
-                    </div>
-                  </div>
-                  <div className="mb-2">
-                    {profile.tags?.map((tag) => (
-                      <Tag
-                        key={tag}
-                        tag={tag}
-                        searchTag={searchTag}
-                        bgColor={bgColor}
-                      />
-                    ))}
-                  </div>
-                  <TitleText
-                    tag="h4"
-                    className="text-sm  md:overflow-hidden md:text-ellipsis md:line-clamp-3"
-                  >
-                    {profile.description}
-                  </TitleText>
-                </SquareBox>
-              </Link>
-            </div>
-          );
-        })}
+        {filteredProfiles.map((profile) => (
+          <ProfileCardItem
+            key={profile._id}
+            _id={profile._id}
+            name={profile.name}
+            image={profile.image}
+            slugAsParams={profile.slugAsParams}
+            description={profile.description}
+            tags={profile.tags}
+            searchTag={searchTag}
+          />
+        ))}
       </div>
     </>
   );
