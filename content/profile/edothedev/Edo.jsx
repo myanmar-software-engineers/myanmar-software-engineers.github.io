cb79b37014c87871bbba45c7e3413a1392314f13
Lwin Moe Paing <lwinmoepaing.dev@gmail.com>
2023-10-22 11:24:47 +0630

    :bug: fix(profile): for contributor profile

--- content/profile/edothedev/Edo.jsx
+++
@@ -1,7 +0,0 @@
-import Spline from '@splinetool/react-spline';
-
-export default function App() {
-    return (
-        <Spline scene="https://prod.spline.design/523zxk5lcIEjBpbL/scene.splinecode" />
-    );
-}
