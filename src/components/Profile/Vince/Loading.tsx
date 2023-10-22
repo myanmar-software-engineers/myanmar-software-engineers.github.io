3af9e158241920d54f5f4512b26d214cfe22de23
Fuck the Error! <74085442+lizzy-km@users.noreply.github.com>
2023-10-23 01:32:34 +0630

    Update Loading.tsx

--- src/components/Profile/Vince/Loading.tsx
+++ src/components/Profile/Vince/Loading.tsx
@@ -1,10 +1,9 @@
-
 const Loading = () => {
   return (
-    <div class=" absolute top-0 left-0 flex flex-col justify-center gap-0 items-center w-full h-screen loader-bg ">
-      <div class=" flex flex-col relative  justify-center items-center w-full  ">
-        <p class="loaderText absolute ">V</p>
-        <span class="loader"></span>
+    <div className=" absolute top-0 left-0 flex flex-col justify-center gap-0 items-center w-full h-screen loader-bg ">
+      <div className=" flex flex-col relative  justify-center items-center w-full  ">
+        <p className="loaderText absolute ">V</p>
+        <span className="loader"></span>
       </div>
     </div>
   );
